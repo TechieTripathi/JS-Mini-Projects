337771bd02fb639b23bbf00d1dd2eb6bbbf0520b
vishnu135468 <vishnu.tripathi.2004@gmail.com>
2024-01-25 20:15:06 +0530

    showing bg-coloe hex code

--- 01_Color_Changing/script.js
+++ 01_Color_Changing/script.js
@@ -1,34 +1,42 @@
     // generate a random color
+    let color = ""
+    
+    if(color == ""){
+      document.getElementById('color-code').style.display = 'none'
+    }
+
     const randomColor = function () {
         const hex = "0123456789ABCDEF";
-        let color = "#";
+        color = "#";
         for (let i = 1; i <= 6; i++) {
           color += hex[Math.floor(Math.random() * 16)];
         }
+        const colorCode = document.createTextNode(`${color}`)
+        document.getElementById('color-code').appendChild(colorCode)
         return color;
       };
   
+
       let intervalId;
-  
+
       const startChangingColor = function () {
         // checking the nullity of intervalID  -> we don't need to disable the start button at all
         if (!intervalId) {
           intervalId = setInterval(changeColor, 1000);
         }
-  
+        
         function changeColor() {
           document.body.style.backgroundColor = randomColor();
         }
         //   document.querySelector("#start").setAttribute("disabled", "");
       };
-  
+      
       const stopChangingColor = function () {
         clearTimeout(intervalId);
         // flushing out the value of intervalId rather than overwriting it
         intervalId = null;
         //   document.querySelector("#start").removeAttribute("disabled");
       };
-  
       // start the interval
       document
         .querySelector("#start")
